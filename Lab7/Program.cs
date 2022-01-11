e775cab49a9e1a587e04b67f73bb267f58418e8e
fox_itleksandr <fox_it@bk.ru>
2022-01-11 15:05:30 +0300

    Add project files.

---
+++ Lab7/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторону A первого треугольника");
+            int sideA1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите сторону B первого треугольника");
+            int sideB1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите сторону C первого треугольника");
+            int sideC1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите сторону A второго треугольника");
+            int sideA2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите сторону B второго треугольника");
+            int sideB2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите сторону C второго треугольника");
+            int sideC2 = Convert.ToInt32(Console.ReadLine());
+
+            if (CalcSquare(sideA1, sideB1, sideC1) == CalcSquare(sideA2, sideB2, sideC2))
+            {
+                Console.WriteLine("треугольники равны");
+            }
+            else
+            {
+                if (CalcSquare(sideA1, sideB1, sideC1) > CalcSquare(sideA2, sideB2, sideC2))
+                {
+                    Console.WriteLine("Больше первый треугольник");
+                }
+                else
+                {
+                    Console.WriteLine("Больше второй треугольник");
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        static double CalcSquare(int sideA, int sideB, int sideC)
+        {
+            double perimeter = sideA + sideB + sideC;
+
+            double p = perimeter / 2;
+
+            double square = Math.Sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+
+            return square;
+        }
+    }
+}
---
+++ Lab7_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра");
+
+            double lenEdge = Convert.ToDouble(Console.ReadLine());
+            double v, s = 0;
+
+            CalcVSCube(lenEdge,out v,out s);
+
+            Console.WriteLine("Объём={0:f2}, площадь поверхности={1:f2}", v, s);
+            Console.ReadKey();
+        }
+
+        static void CalcVSCube(double lenEdge, out double V, out double S)
+        {
+            V = Math.Pow(lenEdge, 3);
+            S = Math.Pow(lenEdge, 2) * 6;
+        }
+    }
+}
